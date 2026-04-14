17ae9d2206c003085f395bc6f3aec7148cb0b9a6
Abhay Mittal <iamabhaymittal@gmail.com>
2026-04-14 15:59:44 +0530

    chore: remove auth call

--- src/app/(marketplace)/business/page.tsx
+++ src/app/(marketplace)/business/page.tsx
@@ -1,14 +1,5 @@
 import { BusinessOverview } from "@/components/dashboard/BusinessOverview";
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 export default async function BusinessDashboardPage() {
-  const session = await getServerSession(authOptions);
-
-  if (!session?.user?.id) {
-    redirect("/auth/sign-in");
-  }
-
   return <BusinessOverview />;
 }
