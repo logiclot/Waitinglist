bf0e35718d25187e1f0d46cccf7b679d0d9fb8c9
Abhay Mittal <iamabhaymittal@gmail.com>
2026-04-14 09:44:05 +0530

    chore: capture trpc errors globally

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,6 +2,8 @@ import { createContext } from "@/lib/trpc/init";
 import { appRouter } from "@/lib/trpc/routers/_app";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { NextRequest } from "next/server";
+import * as Sentry from "@sentry/nextjs";
+
 
 function handler(req: NextRequest) {
     return fetchRequestHandler({
@@ -9,6 +11,22 @@ function handler(req: NextRequest) {
         req,
         router: appRouter,
         createContext: () => createContext(req),
-    });
+        onError({ error, path, type, input }) {
+            // Avoid reporting expected app errors unless you want them in Sentry
+            if (error.code !== "INTERNAL_SERVER_ERROR") return;
+
+            Sentry.withScope((scope) => {
+                scope.setTag("trpc.path", path ?? "unknown");
+                scope.setTag("trpc.type", type);
+                scope.setContext("trpc", {
+                    path,
+                    type,
+                    input,
+                });
+
+                Sentry.captureException(error.cause ?? error);
+            })
+        }
+    })
 }
 export { handler as GET, handler as POST };
