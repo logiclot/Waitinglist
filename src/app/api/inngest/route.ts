55885be1345bbb8ebc41351fc65305dc68ae0315
Abhay Mittal <iamabhaymittal@gmail.com>
2026-04-21 18:07:19 +0530

    chore: move createNotification in background job

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,8 +1,9 @@
 import { inngest } from "@/lib/inngest";
+import { sendNotification } from "@/lib/inngest/tasks/send-notification";
 import { trackLogin } from "@/lib/inngest/tasks/track-login";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [trackLogin],
+    functions: [trackLogin, sendNotification],
 });
