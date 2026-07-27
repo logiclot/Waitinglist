aa78a3a5574bf52fb04edf0acf8fe29c3c9a9e8c
7pxro <i7hepi@gmail.com>
2026-07-27 15:03:28 +0000

    className={`${geistSans.className} ${notoSerif.className}

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -155,7 +155,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
       </head>
       <body
-        className={`${geistSans.variable} ${notoSerif.variable} min-h-screen text-ash-600 antialiased bg-ash-100 flex flex-col`}
+        className={`${geistSans.className} ${notoSerif.className} min-h-screen text-ash-600 antialiased bg-ash-100 flex flex-col`}
       >
         <noscript>
           <iframe
