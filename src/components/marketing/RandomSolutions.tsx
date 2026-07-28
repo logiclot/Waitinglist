2ccf07f9ffbf534c034bb3feccd279d0fa43cb4f
7pxro <i7hepi@gmail.com>
2026-07-28 16:21:58 +0000

    icon

--- src/components/marketing/RandomSolutions.tsx
+++ src/components/marketing/RandomSolutions.tsx
@@ -130,7 +130,7 @@ export function RandomSolutions() {
                 </div>
                 <div className="flex items-center justify-between gap-2 py-2.5 px-6">
                   <div className="flex items-center justify-between gap-4">
-                    <span className="text-sm font-semibold">
+                    <span className="text-sm text-ash-800 font-semibold">
                       €{solution.implementationPrice.toLocaleString()}
                     </span>
                     <span className="text-sm flex items-center gap-1">
