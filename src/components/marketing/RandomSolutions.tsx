10e2ea774459e051af2ad1938f6db0ed87a8631e
7pxro <i7hepi@gmail.com>
2026-07-28 16:40:42 +0000

    className="size-4"

--- src/components/marketing/RandomSolutions.tsx
+++ src/components/marketing/RandomSolutions.tsx
@@ -165,6 +165,7 @@ export function RandomSolutions() {
           <span className="text-ash-800 font-semibold">
             Browse all
           </span>
+          <svg className="size-4 text-ash-800" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
         </Link>
       </div>
     </section>
