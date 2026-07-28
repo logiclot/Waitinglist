0ebce5f2991ec15c98479792002386dbf6e3833a
7pxro <i7hepi@gmail.com>
2026-07-28 19:21:39 +0000

    relative

--- src/components/marketing/FeaturedArticles.tsx
+++ src/components/marketing/FeaturedArticles.tsx
@@ -44,7 +44,7 @@ export function FeaturedArticles() {
   return (
     <section className="pt-20 md:pt-40">
       <div className="container mx-auto px-4 max-w-7xl">
-        <div className="text-center">
+        <div className="text-center relative">
           <span className="text-[70px] font-extrabold tracking-tighter leading-none text-transparent bg-clip-text bg-gradient-to-t from-25% from-transparent to-ash-300 flex justify-center absolute -top-[50px] inset-x-0 md:text-[100px] md:-top-[75px]">
             learn
           </span>
