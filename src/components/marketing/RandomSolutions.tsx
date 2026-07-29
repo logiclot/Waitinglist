5e59342a98f01002eb1386fb3095058854c3dd31
7pxro <i7hepi@gmail.com>
2026-07-29 13:47:58 +0000

    to-ash-400

--- src/components/marketing/RandomSolutions.tsx
+++ src/components/marketing/RandomSolutions.tsx
@@ -62,7 +62,7 @@ export function RandomSolutions() {
     <section className="pt-20 md:pt-40">
       <div className="container mx-auto px-4 xl:px-8 max-w-7xl">
         <div className="text-center relative">
-          <span className="text-[70px] font-extrabold tracking-tighter leading-none text-transparent bg-clip-text bg-gradient-to-t from-25% from-transparent to-ash-300 flex justify-center absolute -top-[50px] inset-x-0 md:text-[100px] md:-top-[75px]">
+          <span className="text-[70px] font-extrabold tracking-tighter leading-none text-transparent bg-clip-text bg-gradient-to-t from-25% from-transparent to-ash-400 flex justify-center absolute -top-[50px] inset-x-0 md:text-[100px] md:-top-[75px]">
             featured
           </span>
           <h2 className="max-w-xl text-2xl text-ash-800 text-balance font-noto font-semibold tracking-tight mx-auto md:text-4xl">
