f9edaddf0ff8ac876b3935354493efa48c43246b
7pxro <i7hepi@gmail.com>
2026-07-29 13:47:22 +0000

    to-ash-400

--- src/components/marketing/ValueGrid.tsx
+++ src/components/marketing/ValueGrid.tsx
@@ -55,7 +55,7 @@ export function ValueGrid() {
     <div>
       {/* Header Secțiune */}
       <div className="text-center relative mb-8">
-        <span className="text-[70px] font-extrabold tracking-tighter leading-none text-transparent bg-clip-text bg-gradient-to-t from-25% from-transparent to-ash-300 flex justify-center absolute -top-[50px] inset-x-0 md:text-[100px] md:-top-[75px]">
+        <span className="text-[70px] font-extrabold tracking-tighter leading-none text-transparent bg-clip-text bg-gradient-to-t from-25% from-transparent to-ash-400 flex justify-center absolute -top-[50px] inset-x-0 md:text-[100px] md:-top-[75px]">
           trust
         </span>
         <h2 className="max-w-md text-2xl text-ash-800 text-balance font-noto font-semibold tracking-tight mx-auto md:text-4xl">
